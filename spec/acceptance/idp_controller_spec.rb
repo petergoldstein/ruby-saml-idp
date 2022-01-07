00543cc19ab650f9102fc5b6c54a3ade68112710
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-01-07 09:36:24 -0800

    More Rubocop changes

--- spec/acceptance/idp_controller_spec.rb
+++ spec/acceptance/idp_controller_spec.rb
@@ -2,23 +2,31 @@
 
 require_relative 'acceptance_helper'
 
-feature 'IdpController' do
+describe 'IdpController', type: :feature do
   let(:idp_port) { 8009 }
   let(:sp_port) { 8022 }
 
-  before do
+  let(:idp_pid) do
     create_app('idp')
+    start_app('idp', idp_port)
+  end
+
+  let(:sp_pid) do
     create_app('sp')
-    @idp_pid = start_app('idp', idp_port)
-    @sp_pid = start_app('sp', sp_port)
+    start_app('sp', sp_port)
+  end
+
+  before do
+    idp_pid
+    sp_pid
   end
 
   after do
-    stop_app('sp', @sp_pid)
-    stop_app('idp', @idp_pid)
+    stop_app('sp', sp_pid)
+    stop_app('idp', idp_pid)
   end
 
-  scenario 'Login via default signup page' do
+  it 'Login via default signup page' do
     saml_request = make_saml_request("http://localhost:#{sp_port}/saml/consume")
     visit "http://localhost:#{idp_port}/saml/auth?SAMLRequest=#{CGI.escape(saml_request)}"
     fill_in 'Email', with: 'brad.copa@example.com'
